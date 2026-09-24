e1e4bcb3e05dc559a7202369934dce13f169a9ac
mengtao <mengtaoxin@outlook.com>
2026-09-24 14:07:04 +0800

    Remove unused SVG file: undraw_docusaurus_tree.svg

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,151 +1,135 @@
 // @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
 
-import { themes as prismThemes } from "prism-react-renderer";
+import {themes as prismThemes} from 'prism-react-renderer';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
-const baseUrl = "/my-website/";
+const baseUrl = '/my-website/';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Site by mengtaoxin",
-  tagline: "Dinosaurs are cool",
-  favicon: "img/favicon.ico",
+  title: 'Site by mengtaoxin',
+  tagline: 'Notes, pages, and images',
+  favicon: 'img/favicon.ico',
   headTags: [
     {
-      tagName: "link",
+      tagName: 'link',
       attributes: {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
         href: `${baseUrl}img/favicon-16x16.png`,
       },
     },
     {
-      tagName: "link",
+      tagName: 'link',
       attributes: {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
         href: `${baseUrl}img/favicon-32x32.png`,
       },
     },
     {
-      tagName: "link",
+      tagName: 'link',
       attributes: {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
         href: `${baseUrl}img/apple-touch-icon.png`,
       },
     },
   ],
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
-  url: "https://mengtaoxin.github.io",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://mengtaoxin.github.io',
   baseUrl,
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "mengtaoxin", // Usually your GitHub org/user name.
-  projectName: "my-website", // Usually your repo name.
+  organizationName: 'mengtaoxin',
+  projectName: 'my-website',
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: 'throw',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          path: "pages",
-          routeBasePath: "pages",
-          sidebarPath: "./sidebars.js",
+          path: 'pages',
+          routeBasePath: 'pages',
+          sidebarPath: './sidebars.js',
         },
         blog: {
           blogSidebarCount: 0,
           postsPerPage: 10,
-          onUntruncatedBlogPosts: "ignore",
+          onUntruncatedBlogPosts: 'ignore',
           showReadingTime: true,
           feedOptions: {
-            type: ["rss", "atom"],
+            type: ['rss', 'atom'],
             xslt: true,
-            title: "Blogs by mengtaoxin",
+            title: 'Blogs by mengtaoxin',
           },
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
         },
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: './src/css/custom.css',
         },
       }),
     ],
   ],
 
-  plugins: ["./src/plugins/homepage-blog"],
+  plugins: ['./src/plugins/homepage-blog'],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      image: "img/icon-512.png",
+      image: 'img/icon-512.png',
       colorMode: {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: "Site by mengtaoxin",
+        title: 'Site by mengtaoxin',
         items: [
           {
-            type: "docSidebar",
-            sidebarId: "tutorialSidebar",
-            position: "left",
-            label: "Pages",
+            type: 'docSidebar',
+            sidebarId: 'pagesSidebar',
+            position: 'left',
+            label: 'Pages',
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: "https://github.com/facebook/docusaurus",
-            label: "GitHub",
-            position: "right",
-          },
-          {
-            href: "https://x.com/dawnlightx123",
-            label: "X",
-            position: "right",
+            href: 'https://github.com/mengtaoxin/my-website',
+            label: 'GitHub',
+            position: 'right',
           },
           {
-            href: "https://steamcommunity.com/id/mengtaoxin/",
-            label: "Steam",
-            position: "right",
+            href: 'https://x.com/dawnlightx123',
+            label: 'X',
+            position: 'right',
           },
           {
-            href: "pathname:///blog/rss.xml",
-            label: "RSS",
-            position: "right",
+            href: 'https://steamcommunity.com/id/mengtaoxin/',
+            label: 'Steam',
+            position: 'right',
+          },
+          {
+            href: 'pathname:///blog/rss.xml',
+            label: 'RSS',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} mengtaoxin.`,
       },
       prism: {
         theme: prismThemes.github,
--- pages/about.md
+++ pages/about.md
@@ -1,2 +1,7 @@
+---
+sidebar_label: About
+---
 
+# About
 
+Personal site of mengtaoxin — notes, pages, and images.
